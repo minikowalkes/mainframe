960682161b1ef22326bd06036f01fe4e840325d8
Cameron Kowalke <minikowalkes@gmail.com>
2025-05-23 15:36:57 -0400

    Updated cwd.sh

--- src/bash/cwd.sh
+++ src/bash/cwd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # cwd = copy working directory
 # for use in conjunction with .bash_aliases
-echo `pwd` `date` >> ~/.places.txt
+# echo `pwd` `date` >> ~/.places.txt << prior version, dated command substitution syntax. not very unixy anymore.
+echo $(pwd) $(date) >> ~/.places.txt
