3a9643ff4e3a2e8041d65b8244b916304562efbb
Cameron Kowalke <minikowalkes@gmail.com>
2025-05-23 15:30:04 -0400

    Created hello.c

---
+++ hello_worlds/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// Have no idea if this'll work but it was almost(99%) copied word-for-word
+// From the ANSI C standard book from 1980-whatever.
+main() {
+  printf("Hello World!\n");
+}
+// As you can see, currently no idea how to do a class in C-based languages.
+// Stick around to see if i figure it out.
