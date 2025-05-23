b416e99f2dca7bb63404d711d5a8c4a4e8ae2070
Cameron Kowalke <minikowalkes@gmail.com>
2025-05-23 15:22:13 -0400

    Created helloWorld.cpp

---
+++ hello_worlds/helloWorld.cpp
@@ -0,0 +1,9 @@
+#include <stdio>
+
+int main() 
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
+// This may not compile due to github auto-formatting. 
+// I don't use namespace std.
