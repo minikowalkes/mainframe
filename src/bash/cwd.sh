286af61e951f58c5fa674dd405ccb4ec0acfca47
Cameron Kowalke <minikowalkes@gmail.com>
2025-05-25 15:56:48 -0400

    Deleted src/bash/cwd.sh
    Final version is hidden in the user's home directory.

--- src/bash/cwd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# cwd = copy working directory
-# for use in conjunction with .bash_aliases
-# echo `pwd` `date` >> ~/.places.txt << prior version, dated command substitution syntax. not very unixy anymore.
-echo $(pwd) $(date) >> ~/.places.txt
