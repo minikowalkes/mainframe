531863adabdbeab7ee475d30663ce93fdd2b4a1c
Cameron Kowalke <minikowalkes@gmail.com>
2025-07-11 13:17:24 -0400

    Created isbn_checker.py

---
+++ src/python/isbn_checker.py
@@ -0,0 +1,34 @@
+# isbn_checker .py
+# version 0.1
+'''TODO:
+   1) Figure out re. Too lazy to do so right now.
+   2) Figure out some webscraping nonsense to compare
+      the text of each book.
+   3) Turning this into part of the regular expression chemistry
+      parser turing machine garbage project that's a total dumpster
+      fire. Can't stop the unhinged comments in my code lol.
+   4) Also going to need some sort of web address search algorithm. 
+      Needs to search a few common book re-selling websites.
+   5) What's a python?
+   6) This is literally just so that I don't waste any money
+      on a textbook that I don't need. I literally just want
+      to confirm the book, but since they have different covers,
+      they have different ISBNs and I can't f***in' see a
+      preview of the d**n book. I'm literally going insane.''''
+
+def main():
+  isbn_1 = int(input("Enter first ISBN with no dashes:  "))
+  isbn_2 = int(input("Enter second ISBN with no dashes:  "))
+  if num1 == num2:
+    print("Same.")
+  else:
+    print("Different.")
+
+main()
+
+'''Okay so now we need some sort of webscraper
+   to compare ISBN's to actual text present within
+   the books so this is garbage and I hate python
+   more than life itself.
+
+   'Tis but a scratch.'''
