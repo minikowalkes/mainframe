885a9c3533d114d08a5ade55ad2ec5f3953408e2
Cameron Kowalke <minikowalkes@gmail.com>
2025-07-12 16:30:20 -0400

    Created sys_mod_learn.py
    Reference

---
+++ src/learning/sys_mod_learn.py
@@ -0,0 +1,38 @@
+#import sys <=== this module yippie
+'''7/12/25 "version 0.1"
+
+   Basically just stared at the interactive help
+   screen for an hour straight and tried to talk
+   myself through it. No more oop today, I wanna
+   make art lol.'''
+
+# stdin ===> keyboard, mouse, peripherals, etc.
+# stdout ===> monitor, speakers, peripherals, etc.
+# stderr ===> ? do not yet understand.
+
+# DO NOT MESS WITH THESE DUNDER METHODS
+'''__stdin__
+   __stdout__
+   __stderr__
+   __displayhook__
+   __excepthook__'''
+
+# FUN FUNCTIONS
+'''displayhook() -> prints obj to screen, saves in builtins.
+   excepthook() -> print error message to sys.stderr
+   exception() -> return current thread error/exception
+   -> also returns most recent exception via except keyword
+   -> somethin' somethin' stack frames
+   
+   exc_info() -> return current thread error/exception
+   exit() -> exit via raising canes/// SystemExit
+   -> exit(status=None, /) <=== default param.
+
+   dlopen() -> ? do not yet understand
+   getdlopenflags() -> returns flags to be used for line 22
+   ...bunch of get methods
+
+   LEFT OFF AT
+   getprofile() method.'''
+
+# what is the "Library Manual?!?!"
