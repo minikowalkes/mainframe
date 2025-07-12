4a78bdb1b1aff7623583909025a740415aa848cd
Cameron Kowalke <minikowalkes@gmail.com>
2025-07-12 16:32:30 -0400

    Updated HelloWorld.java
    Spaghetti-fied it.

--- hello_worlds/HelloWorld.java
+++ hello_worlds/HelloWorld.java
@@ -1,5 +1,3 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.printf("Hello world!\n");
-  }
-}
+public class HelloWorld {public static void main(String[] args) {System.out.printf("Hello world!\n");}}
+// Recently learned that Java doesn't care about whitespace, depending on interpreter/JDK/whatever-ville.
+// Get ready for some spaghetti code lol.
