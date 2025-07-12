8573d54c8c00de76594a2d471579ae73ee8ca18c
Cameron Kowalke <minikowalkes@gmail.com>
2025-07-11 22:57:13 -0400

    Updated inefficient_word_search.py

--- src/python/inefficient_word_search.py
+++ src/python/inefficient_word_search.py
@@ -1,14 +1,13 @@
-# inefficient word search
-# version 0.1
-# another tester script, word counter
+# inefficient word "search"
+# version 0.2 (changed some variable names, commented out function call.)
+# another tester script, word counter <= more accurate descriptor.
 def thingy(a: list) -> int:
     return int(len(a))
 
 def thingimajig():
     # gee, the locals sure are friendly
-    putter = str(input("Enter a string to count:  "))
-    # no idea how python will interpret this.
-    garbo = thingy(putter)
-    print(garbo)
+    silly_putty = str(input("Enter a string to count:  "))
+    garbage = thingy(silly_putty)
+    print(garbage)
 
-thingimajig()
+#thingimajig()   <=== Uncomment as necessary.
