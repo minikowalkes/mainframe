9c1bec30deb984f2243976dcd3b6553156854e51
Primus Fan <minikowalkes@gmail.com>
2025-07-14 15:01:28 -0400

    Created re_learn_finally.py
    FINALLY!

---
+++ src/learning/re_learn_finally.py
@@ -0,0 +1,35 @@
+import re  #     <=== obviously necessary
+'''
+   7/14/25
+   period => matches any char except newline
+   carrot => matches beginning of string
+   dollar sign => matches end of string, or newline
+   asterisk => ...? 0 or more "greedy repetitions?"     <=== come back to this...
+   plus sign => ...? 1 or more "greedy repetitions?"    <=== come back to this...
+   question mark => ...? 0 or 1 "greedy repetitions?"   <=== come back to this...
+
+   NOTES: Look up what is meant by "greedy repetition,"
+   most if not all of these also require the prior
+   existence of another regular expression.
+
+   From my current understanding, I would assume that
+   "greedy repetition" infers to a time-saving way
+   to write previous regexs? But we all know what
+   happens when we assume...
+
+   MORE NOTES: Maybe it is a mistake to learn the
+   re python module as opposed to bash regex 
+   grammar? Only time will tell.
+
+   That's all for today. Will eventually return
+   to sys/os/pathlib and pygame. Pygame is a
+   higher priority right now, though.
+
+   Of course, the obligatory reference to media. This
+   time, it's a spongebob quote lol.
+   FINALLY! I've been trying to reach you boys ALL DAY! 
+   And now that I've got you right where I want you... 
+   I'd like to buy all your chocolate!
+   
+   --minikowalkes
+'''
